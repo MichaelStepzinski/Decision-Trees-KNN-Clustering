e65cfa082c82f71acc076d4ddf69b0bffb0e535f
DesertPangolin <michaelpstepzinski@gmail.com>
2021-09-23 16:24:21 -0700

    updates

--- Workspace/decision_trees.py
+++ Workspace/decision_trees.py
@@ -9,6 +9,7 @@ def DT_train_binary(X, Y, max_depth):
     set_length = len(Y)
     num_positive = sum(Y)
     total_entropy = calculate_entropy(num_positive, set_length)
+    branch_entropies = []
 
     # step 2 - split training data using each feature and calculate information gain for each split
     # print("{} {}".format(idx[1], Y[idx[0]])) for printing entire table format
@@ -17,27 +18,58 @@ def DT_train_binary(X, Y, max_depth):
 
     num1 = find_split_entropy(0, 0, X, Y)
     num2 = find_split_entropy(0, 1, X, Y)
+    branch_entropies.append(find_IG(num1, num2, total_entropy, set_length))
 
-    print(find_IG(num1, num2, total_entropy, set_length))
+    #num1 = find_split_entropy(1, 0, X, Y)
+    #num2 = find_split_entropy(1, 1, X, Y)
+    #branch_entropies.append(find_IG(num1, num2, total_entropy, set_length))
 
-    num1 = find_split_entropy(1, 0, X, Y)
-    num2 = find_split_entropy(1, 1, X, Y)
+    #num1 = find_split_entropy(2, 0, X, Y)
+    #num2 = find_split_entropy(2, 1, X, Y)
+    #branch_entropies.append(find_IG(num1, num2, total_entropy, set_length))
 
-    print(find_IG(num1, num2, total_entropy, set_length))
+    #num1 = find_split_entropy(3, 0, X, Y)
+    #num2 = find_split_entropy(3, 1, X, Y)
+    #branch_entropies.append(find_IG(num1, num2, total_entropy, set_length))
 
-    num1 = find_split_entropy(2, 0, X, Y)
-    num2 = find_split_entropy(2, 1, X, Y)
+    #for x in branch_entropies:
+    #    print(x)
 
-    print(find_IG(num1, num2, total_entropy, set_length))
+    # step 3 - split on feature that gives highest IG
+    #highest_IG = max(branch_entropies)
+    #max_index = branch_entropies.index(highest_IG)
+    #print(max_index)
 
-    num1 = find_split_entropy(3, 0, X, Y)
-    num2 = find_split_entropy(3, 1, X, Y)
+    # repeat 2 - 4
+    # find new set length and entropy splitting on max_index thus far
+    total_entropy, set_length = find_split_entropy(max_index, 1, X, Y)
+    #print(total_entropy)
+    #print(set_length)
 
-    print(find_IG(num1, num2, total_entropy, set_length))
+    #tuple = find_split_entropy(max_index, 0, X, Y)
+    #index = -1
+    #finished = []
+    #if(tuple[0] == 0):
+        #list entries that have been calculated already
+    #    for itx in X:
+    #        index = index + 1
+    #        if(itx[max_index] == 0):
+    #            finished.append(index)
+    #print(finished)
 
+
+def test_accuracy(max_index, set_length, X, Y):
+    accuracy = 0
+    for idx in enumerate(X):
+        if idx[1][max_index] == 1 and Y[idx[0]] == 1:
+            accuracy = accuracy + 1
+        elif idx[1][max_index] == 0 and Y[idx[0]] == 0:
+            accuracy = accuracy + 1
+    return accuracy / set_length
 
 def find_IG(splitL, splitR, total_entropy, length):
-    return total_entropy - ((splitL[1]/length)*splitL[0] + (splitR[1]/length)*splitR[0])
+    return total_entropy - ((splitL[1] / length) * splitL[0] + (splitR[1] / length) * splitR[0])
+
 
 def find_split_entropy(column, value, X, Y):
     split_positive = 0
